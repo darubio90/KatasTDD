640e358fe972818f614b38cdea233e748c8bc413
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-18 15:12:29 -0500

    🟥Si_Anio_4_Es_Divisible_Entre_4_Retornar_True

--- Katas/LeapYear/LeapYearUnitTest.cs
+++ Katas/LeapYear/LeapYearUnitTest.cs
@@ -1,6 +1,24 @@
-﻿namespace LeapYear
+﻿
+using FluentAssertions;
+
+namespace LeapYear
 {
     public class LeapYearUnitTest
     {
+        [Fact]
+        public void Si_Anio_4_Es_Divisible_Entre_4_Retornar_True()
+        {
+            bool esBisiesto = Anio.EsBisiesto(4);
+            esBisiesto.Should().BeTrue();
+
+        }
+    }
+
+    public class Anio
+    {
+        public static bool EsBisiesto(int anio)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
