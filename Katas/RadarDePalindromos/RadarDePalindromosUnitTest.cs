d80e53b419c0226f424a78bd7318c2681066adb7
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-18 18:12:08 -0500

    🟥Si_Cadena_Es_Vacio_Debe_Retornar_Vacio

---
+++ Katas/RadarDePalindromos/RadarDePalindromosUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RadarDePalindromos
+{
+    public class RadarDePalindromosUnitTest
+    {
+        [Fact]
+        public void Si_Cadena_Es_Vacio_Debe_Retornar_Vacio()
+        {
+            string cadena = QuitarEspacionCadena("");
+            cadena.Should().Be(string.Empty);
+        }
+
+        private string QuitarEspacionCadena(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
