6ace0180872567b3c8c3885b93f94f0ac74c5578
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-18 18:15:50 -0500

    🟥Si_Cadena_Tiene_Un_Espacion_Debe_Retornar_Cadena_Sin_Espacios

--- Katas/RadarDePalindromos/RadarDePalindromosUnitTest.cs
+++ Katas/RadarDePalindromos/RadarDePalindromosUnitTest.cs
@@ -12,6 +12,13 @@ namespace RadarDePalindromos
             cadena.Should().Be(string.Empty);
         }
 
+        [Fact]
+        public void Si_Cadena_Tiene_Un_Espacion_Debe_Retornar_Cadena_Sin_Espacios()
+        {
+            string cadena = QuitarEspacionCadena(" a");
+            cadena.Should().Be("a");
+        }
+
         private string QuitarEspacionCadena(string cadena)
         {
             return string.Empty;
