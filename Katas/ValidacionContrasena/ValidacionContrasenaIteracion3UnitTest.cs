63d8ef89922f212579fdc128ed8eadb7ba36dfc0
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 17:12:30 -0500

    🔴Si_ContraseñaNoTieneMinusculas_Debe_Retonar_False

--- Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
+++ Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
@@ -31,6 +31,20 @@ namespace ValidacionContrasena
 
             esValida.Should().BeFalse();
         }
+
+        [Fact]
+        public void Si_ContraseñaNoTieneMinusculas_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion3("PASSSSSSSWORDDDDDDDDDDDDDDDD");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+        }
     }
 
     public class GestorContrasenaIteracion3 : GestorContrasenaBase
