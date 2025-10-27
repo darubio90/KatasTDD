6a55794144bec16b01114115b3a41e41d63bd740
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 18:03:46 -0500

    🔵Se saca a nuevo archivo clase GestorContrasenaIteracion3

---
+++ Katas/ValidacionContrasena/GestorContrasenaIteracion3.cs
@@ -0,0 +1,19 @@
+﻿namespace ValidacionContrasena
+{
+    public class GestorContrasenaIteracion3 : GestorContrasenaBase
+    {
+        public GestorContrasenaIteracion3(string contrasena) : base(contrasena, limiteCaracteresContrasena: 16)
+        {
+        }
+
+        public override bool EsValida()
+        {
+            if (LaContrasenaNoCumpleConLosCaracteresMinimos() is false
+                || LaContrasenaNoContieneMayusculas() is false
+                || LaContrasenaNoContieneMinusculas() is false
+                || LaContrasenaNoTieneGuionBajo() is false)
+                return false;
+            return true;
+        }
+    }
+}
--- Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
+++ Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
@@ -74,21 +74,4 @@ namespace ValidacionContrasena
             esValida.Should().BeTrue();
         }
     }
-
-    public class GestorContrasenaIteracion3 : GestorContrasenaBase
-    {
-        public GestorContrasenaIteracion3(string contrasena) : base(contrasena, limiteCaracteresContrasena: 16)
-        {
-        }
-
-        public override bool EsValida()
-        {
-            if (LaContrasenaNoCumpleConLosCaracteresMinimos() is false
-                || LaContrasenaNoContieneMayusculas() is false
-                || LaContrasenaNoContieneMinusculas() is false
-                || LaContrasenaNoTieneGuionBajo() is false)
-                return false;
-            return true;
-        }
-    }
 }
