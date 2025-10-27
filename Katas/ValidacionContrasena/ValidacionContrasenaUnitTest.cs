618131188a9e00c5f7207edf0323643d64f0a858
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 14:36:25 -0500

    🔴Si_ContraseñaTieneMenosDeOchoCaracteres_Debe_Retonar_False

---
+++ Katas/ValidacionContrasena/ValidacionContrasenaUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidacionContrasena
+{
+    public class ValidacionContrasenaUnitTest
+    {
+        [Fact]
+        public void Si_ContraseñaTieneMenosDeOchoCaracteres_Debe_Retonar_False()
+        {
+            //arrange
+            var gestorContrasena = new GestorContrasena("pass123");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+
+        }
+    }
+
+    public class GestorContrasena
+    {
+        private string v;
+
+        public GestorContrasena(string v)
+        {
+            this.v = v;
+        }
+
+        public bool EsValida()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
