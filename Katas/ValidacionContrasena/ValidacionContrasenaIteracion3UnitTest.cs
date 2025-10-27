27d60d20681606b54eff95c994b8def2e65c735c
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 16:45:57 -0500

    🟢Si_ContraseñaTieneMenosDeDieciseisCaracteres_Debe_Retonar_False

--- Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
+++ Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
@@ -1,9 +1,4 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ValidacionContrasena
 {
@@ -34,7 +29,11 @@ namespace ValidacionContrasena
 
         public override bool EsValida()
         {
-            throw new NotImplementedException();
+            if (Contrasena.Length < LimiteCaracteresContrasena)
+                return false;
+            return true;
+
+
         }
     }
 }
