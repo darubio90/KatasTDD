51390cf6ae4eea9a3a3a0aba44a66dc1662c71ab
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 15:27:54 -0500

    🔵Se crea clase base y se crea clase GestorContrasenaIteracion1 y rename en clase test para iteracion1 y ajuste en test

---
+++ Katas/ValidacionContrasena/GestorContrasenaIteracion1.cs
@@ -0,0 +1,61 @@
+﻿namespace ValidacionContrasena
+{
+
+    public abstract class GestorContrasenaBase
+    {
+        public abstract bool EsValida();
+    }
+
+    public class GestorContrasenaIteracion1 : GestorContrasenaBase
+    {
+        private const int LimiteCaracteresContrasena = 9;
+        private string Contrasena;
+
+        public GestorContrasenaIteracion1(string contrasena)
+        {
+            Contrasena = contrasena;
+        }
+
+        public override bool EsValida()
+        {
+            if (LaContrasenaNoCumpleConLosCaracteresMinimos()
+                || LaContrasenaNoContieneMayusculas() is false
+                || LaContrasenaNoContieneMinusculas() is false
+                || LaContrasenaNoTieneNumeros() is false
+                || LaContrasenaNoTieneGuionBajo() is false)
+                return false;
+
+            return true;
+        }
+
+        private bool LaContrasenaNoTieneGuionBajo()
+        {
+            return Contrasena.Contains("_");
+        }
+
+        private bool LaContrasenaNoTieneNumeros()
+        {
+            return ValidarCon(caracter => char.IsNumber(caracter));
+        }
+
+        private bool LaContrasenaNoContieneMinusculas()
+        {
+            return ValidarCon(caracter => char.IsLower(caracter));
+        }
+
+        private bool LaContrasenaNoContieneMayusculas()
+        {
+            return ValidarCon(caracter => char.IsUpper(caracter));
+        }
+
+        private bool LaContrasenaNoCumpleConLosCaracteresMinimos()
+        {
+            return ValidarCon(caracter => caracter < LimiteCaracteresContrasena);
+        }
+
+        public bool ValidarCon(Func<char, bool> predicado)
+        {
+            return Contrasena.Any(predicado);
+        }
+    }
+}
---
+++ Katas/ValidacionContrasena/ValidacionContrasenaIteracion1UnitTest.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+
+namespace ValidacionContrasena
+{
+    public class ValidacionContrasenaIteracion1UnitTest
+    {
+        [Fact]
+        public void Si_ContraseñaTieneMenosDeOchoCaracteres_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("pass123");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void Si_ContraseñaNoTieneMayusculas_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("pass1234567");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void Si_ContraseñaNoTieneMinusculas_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("PASS1234567");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void Si_ContraseñaNoTieneNumeros_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("Passwordddd");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void Si_ContraseñaNoTieneGuionBajo_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("Passwordddd1");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Si_ContraseñaEsValida_Debe_Retonar_True()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion1("Passwo_rdddd1");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeTrue();
+
+        }
+    }
+}
