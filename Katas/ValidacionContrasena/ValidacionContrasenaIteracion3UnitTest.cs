f4f90e84acad8de40d9d016992280c4724247da8
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-10-27 16:41:13 -0500

    🔴Si_ContraseñaTieneMenosDeDieciseisCaracteres_Debe_Retonar_False

---
+++ Katas/ValidacionContrasena/ValidacionContrasenaIteracion3UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionContrasena
+{
+    public class ValidacionContrasenaIteracion3UnitTest
+    {
+        [Fact]
+        public void Si_ContraseñaTieneMenosDeDieciseisCaracteres_Debe_Retonar_False()
+        {
+            //arrange
+            GestorContrasenaBase gestorContrasena = new GestorContrasenaIteracion3("passwroddddrdfg");
+
+            //act
+            bool esValida = gestorContrasena.EsValida();
+
+            //assert
+
+            esValida.Should().BeFalse();
+        }
+    }
+
+    public class GestorContrasenaIteracion3 : GestorContrasenaBase
+    {
+        private string v;
+
+        public GestorContrasenaIteracion3(string contrasena) : base(contrasena, limiteCaracteresContrasena: 16)
+        {
+        }
+
+        public override bool EsValida()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
