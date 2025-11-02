483002ec530715ad6a5b2a4ca38ef4e55db642f5
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-02 16:23:18 -0500

    🔴SiElCajeroExiste_Debe_CrearseConDinero

---
+++ Katas/Cajero/CajeroUnitTest.cs
@@ -0,0 +1,33 @@
+﻿
+using FluentAssertions;
+
+namespace CajeroDinero
+{
+    public class CajeroUnitTest
+    {
+        [Fact]
+        public void SiElCajeroExiste_Debe_CrearseConDinero()
+        {
+            //arrange
+            var cajero = new Cajero();
+
+            //act
+            bool tieneDinero = cajero.TieneDinero();
+            //assert
+            tieneDinero.Should().BeTrue();
+
+        }
+    }
+
+    public class Cajero
+    {
+        public Cajero()
+        {
+        }
+
+        public bool TieneDinero()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
