32254700e5ab2d83df9eeaff0a210f9c108a4d9e
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-03 10:32:00 -0500

    🔴ElCajero_Debe_CrearseConDinero

--- Katas/Cajero/CajeroUnitTest.cs
+++ Katas/Cajero/CajeroUnitTest.cs
@@ -6,10 +6,14 @@ namespace CajeroDinero
     public class CajeroUnitTest
     {
         [Fact]
-        public void SiElCajeroExiste_Debe_CrearseConDinero()
+        public void ElCajero_Debe_CrearseConDinero()
         {
+            List<Dinero> dineroIngresado = new()
+            {
+                new(500,1,Tipo.Billete)
+            };
             //arrange
-            var cajero = new Cajero();
+            var cajero = new Cajero(dineroIngresado);
 
             //act
             bool tieneDinero = cajero.TieneDinero();
@@ -19,10 +23,33 @@ namespace CajeroDinero
         }
     }
 
+    public record Dinero
+    {
+        public Dinero(int valor, int unidades, Tipo tipo)
+        {
+            Valor = valor;
+            Unidades = unidades;
+            Tipo = tipo;
+        }
+
+        public int Valor { get; set; }
+        public int Unidades { get; set; }
+        public Tipo Tipo { get; set; }
+    }
+
+    public enum Tipo
+    {
+        Billete,
+        Moneda
+    }
+
     public class Cajero
     {
-        public Cajero()
+        public List<Dinero> Saldo { get; set; }
+        public Cajero(List<Dinero> saldo)
         {
+            Saldo = saldo;
+
         }
 
         public bool TieneDinero()
