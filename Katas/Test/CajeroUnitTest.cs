90ac3d4ecd1a8e0f32d80e0ad157b0910b3ce437
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-06 16:15:38 -0500

    🔴Si_Solicito1000AlCajero_Debe_DevolverBilleteDe1000

--- Katas/Test/CajeroUnitTest.cs
+++ Katas/Test/CajeroUnitTest.cs
@@ -1,11 +1,68 @@
-﻿namespace Test
+﻿using FluentAssertions;
+
+namespace Test
 {
     public class CajeroUnitTest
     {
         [Fact]
-        public void Test1()
+        public void Si_Solicito1000AlCajero_Debe_DevolverBilleteDe1000()
         {
 
+            //arrange
+            var cajero = new Cajero();
+            //act
+            List<Dinero> dineroActual = cajero.Retirar(500);
+
+            //assert
+            List<Dinero> dineroEsperado = new() { new(500, 1, Tipo.Billete) };
+
+            dineroActual.Should().Equal(dineroEsperado);
         }
+    }
+
+    public class Cajero
+    {
+        private List<Dinero> Saldo { get; }
+        public Cajero()
+        {
+            Saldo = new List<Dinero>()
+            {
+                new (500,1,Tipo.Billete),
+                new (200,1,Tipo.Billete),
+                new (100,1,Tipo.Billete),
+                new (50,1,Tipo.Billete),
+                new (20,1,Tipo.Billete),
+                new (10,1,Tipo.Billete),
+                new (5,1,Tipo.Billete),
+                new (2,1,Tipo.Moneda),
+                new (1,1,Tipo.Moneda),
+
+            };
+        }
+
+        internal List<Dinero> Retirar(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record Dinero
+    {
+        public Dinero(int valor, int unidad, Tipo tipo)
+        {
+            Valor = valor;
+            Unidad = unidad;
+            Tipo = tipo;
+        }
+
+        public int Valor { get; set; }
+        public int Unidad { get; set; }
+        public Tipo Tipo { get; set; }
+    }
+
+    public enum Tipo
+    {
+        Billete,
+        Moneda
     }
 }
