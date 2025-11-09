4bffd87a5bf3d403c0d9b164360142bd1c798ef5
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-08 22:39:33 -0500

    🔵Si_CreoCompraConCantidadEnCero_Debe_LanzarExcepcion

--- Katas/Supermercado/CompraUnitTest.cs
+++ Katas/Supermercado/CompraUnitTest.cs
@@ -8,7 +8,7 @@ namespace Supermercado
         public void Si_CreoCompraConCantidadEnNegativa_Debe_LanzarExcepcion()
         {
             var producto = Producto.Crear("Leche", 5000);
-            var excepcion = () => new Compra(producto, -5);
+            var excepcion = () => Compra.Crear(producto, -5);
 
             excepcion.Should().Throw<Exception>().WithMessage("*La cantidad no puede ser negativa");
         }
@@ -17,7 +17,7 @@ namespace Supermercado
         public void Si_CreoCompraConCantidadEnCero_Debe_LanzarExcepcion()
         {
             var producto = Producto.Crear("Leche", 5000);
-            var excepcion = () => new Compra(producto, 0);
+            var excepcion = () => Compra.Crear(producto, 0);
 
             excepcion.Should().Throw<Exception>().WithMessage("*La cantidad no puede ser cero");
         }
@@ -29,15 +29,19 @@ namespace Supermercado
         private Producto Producto { get; }
         private int Cantidad { get; }
 
-        public Compra(Producto producto, int cantidad)
+        public static Compra Crear(Producto producto, int cantidad)
         {
-            LanzarExcepcionSiCantidadEsNegativa(cantidad);
+            LanzarExcepcionSiCantidadNoEsValida(cantidad);
+            return new Compra(producto, cantidad);
+        }
 
+        private Compra(Producto producto, int cantidad)
+        {
             Producto = producto;
             Cantidad = cantidad;
         }
 
-        private static void LanzarExcepcionSiCantidadEsNegativa(int cantidad)
+        private static void LanzarExcepcionSiCantidadNoEsValida(int cantidad)
         {
             if (cantidad < 0)
                 throw new Exception("La cantidad no puede ser negativa");
