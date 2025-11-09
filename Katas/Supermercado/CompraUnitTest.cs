3c25ec0a8180a08c9ba965b53f259ff647d4d733
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-08 22:39:54 -0500

    🔵Se crea clase Compra

--- Katas/Supermercado/CompraUnitTest.cs
+++ Katas/Supermercado/CompraUnitTest.cs
@@ -23,31 +23,4 @@ namespace Supermercado
         }
 
     }
-
-    public class Compra
-    {
-        private Producto Producto { get; }
-        private int Cantidad { get; }
-
-        public static Compra Crear(Producto producto, int cantidad)
-        {
-            LanzarExcepcionSiCantidadNoEsValida(cantidad);
-            return new Compra(producto, cantidad);
-        }
-
-        private Compra(Producto producto, int cantidad)
-        {
-            Producto = producto;
-            Cantidad = cantidad;
-        }
-
-        private static void LanzarExcepcionSiCantidadNoEsValida(int cantidad)
-        {
-            if (cantidad < 0)
-                throw new Exception("La cantidad no puede ser negativa");
-
-            if (cantidad == 0)
-                throw new Exception("La cantidad no puede ser cero");
-        }
-    }
 }
