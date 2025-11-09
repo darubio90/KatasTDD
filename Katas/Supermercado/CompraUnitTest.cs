d870892d029c114e97c8788095875af39926d151
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-08 22:34:01 -0500

    🔴Si_CreoCompraConCantidadEnNegativa_Debe_LanzarExcepcion

---
+++ Katas/Supermercado/CompraUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace Supermercado
+{
+    public class CompraUnitTest
+    {
+        [Fact]
+        public void Si_CreoCompraConCantidadEnNegativa_Debe_LanzarExcepcion()
+        {
+            var producto = Producto.Crear("Leche", 5000);
+            var excepcion = () => new Compra(producto, -5);
+
+            excepcion.Should().Throw<Exception>().WithMessage("*La cantidad no puede ser negativa");
+        }
+
+    }
+
+    public class Compra
+    {
+        private Producto Producto { get; }
+        private int Cantidad { get; }
+
+        public Compra(Producto producto, int cantidad)
+        {
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+    }
+}
