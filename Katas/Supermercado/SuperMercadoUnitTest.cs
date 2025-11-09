7f3c408d6e75adcbcff1b5c8cf2c4b529561ab66
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-08 22:15:20 -0500

    🔵Si_CreoProductoConNombreVacio_Debe_LanzarExpcecion

--- Katas/Supermercado/SuperMercadoUnitTest.cs
+++ Katas/Supermercado/SuperMercadoUnitTest.cs
@@ -7,7 +7,7 @@ namespace Supermercado
         [Fact]
         public void Si_CreoProductoConNombreVacio_Debe_LanzarExpcecion()
         {
-            var excepcion = () => new Producto("", 1000);
+            var excepcion = () => Producto.Crear("", 1000);
 
             excepcion.Should().Throw<Exception>().WithMessage("*El producto no puede crearse sin nombre");
         }
@@ -17,13 +17,24 @@ namespace Supermercado
     {
         private string Nombre { get; }
         private decimal Precio { get; }
-        public Producto(string nombre, decimal precio)
+
+
+        public static Producto Crear(string nombre, decimal valor)
+        {
+            LanzarExcepcionSiNombreEsNuloOVacio(nombre);
+            return new Producto(nombre, valor);
+        }
+
+        private Producto(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        private static void LanzarExcepcionSiNombreEsNuloOVacio(string nombre)
         {
             if (string.IsNullOrEmpty(nombre))
                 throw new Exception("El producto no puede crearse sin nombre");
-
-            Nombre = nombre;
-            Precio = precio;
         }
     }
 }
