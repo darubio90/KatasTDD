97cbd677118571077644ab68ee37b136ab02b3e3
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-08 22:28:36 -0500

    🔵Se crea clase Producto

---
+++ Katas/Supermercado/Producto.cs
@@ -0,0 +1,38 @@
+﻿namespace Supermercado
+{
+    public class Producto
+    {
+        private string Nombre { get; }
+        private decimal Precio { get; }
+
+
+        public static Producto Crear(string nombre, decimal valor)
+        {
+            LanzarExcepcionSinValorNoEsValido(valor);
+            LanzarExcepcionSiNombreEsNuloOVacio(nombre);
+            return new Producto(nombre, valor);
+        }
+
+        private static void LanzarExcepcionSinValorNoEsValido(decimal valor)
+        {
+            const int cero = 0;
+            if (valor < cero)
+                throw new Exception("El producto no puede crearse con valor negativo");
+
+            if (valor == cero)
+                throw new Exception("El producto no puede crearse con valor en cero");
+        }
+
+        private Producto(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        private static void LanzarExcepcionSiNombreEsNuloOVacio(string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new Exception("El producto no puede crearse sin nombre");
+        }
+    }
+}
