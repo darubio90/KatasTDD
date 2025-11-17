00f81e917eb7e603802ef724bdbbcdcafe88c2c4
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-17 15:52:22 -0500

    🔴Si_LaPalabraEsVacioYElSaltoDeLineaEs1_Debe_Mostrar_Vacio

--- Katas/WordWrapTest/WordWrapUnitTest.cs
+++ Katas/WordWrapTest/WordWrapUnitTest.cs
@@ -1,11 +1,30 @@
-﻿namespace WordWrapTest
+﻿using FluentAssertions;
+
+namespace WordWrapTest
 {
     public class WordWrapUnitTest
     {
         [Fact]
-        public void Test1()
+        public void Si_LaPalabraEsVacioYElSaltoDeLineaEs1_Debe_Mostrar_Vacio()
         {
+            // arrange
+            string palabra = "";
 
+            // act
+            var resultado = Word.Wrap(palabra, 1);
+
+            //assert
+
+            resultado.Should().Be("");
+
+        }
+    }
+
+    public class Word
+    {
+        public static string Wrap(string palabra, int v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
